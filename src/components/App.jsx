202ec9167014f896528d6293e21a3d40c229ab13
Naidaly ruiz <naidalyruiz@gmail.com>
2024-02-08 18:42:25 +0100

    files tructure, initial commit

---
+++ src/components/App.jsx
@@ -0,0 +1,12 @@
+import "../styles/_App.scss";
+import { Route, Routes } from "react-router-dom";
+
+function App() {
+  return (
+    <div>
+      <h2>Este título aparece siempre</h2>
+    </div>
+  );
+}
+
+export default App;
